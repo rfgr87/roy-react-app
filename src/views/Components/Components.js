2764c998c73e0e56fc6f60a31f592d60f8879c59
Roy F Guzman <royguzrod@gmail.com>
2021-04-18 00:54:29 -0400

    modified

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -17,7 +17,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 
-import image1 from "/Users/royguzman/Documents/My Web Apps 2/My Web Projects/roy-react-app/src/roy-fotos/glitch3.png";
+import image1 from './glitch3.png';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
