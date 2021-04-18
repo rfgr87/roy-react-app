4d5e35701d54c6df404f225ea807a4e1302911a3
Roy F Guzman <royguzrod@gmail.com>
2021-04-18 04:42:18 -0400

    modified

--- src/views/ProfilePage/ProfilePage1.js
+++ src/views/ProfilePage/ProfilePage1.js
@@ -18,8 +18,8 @@ import {
 // import GridContainer from "components/Grid/GridContainer.js";
 // import GridItem from "/src/components/Grid/GridItem.js";
 
-var image1 = require("./TainoHombreFractal.png");
-var image2 = require("./roy1.jpg");
+import image1 from "./TainoHombreFractal.png";
+import image2 from "./roy1.jpg";
 
 
 export default function ProfilePage1(props) {
